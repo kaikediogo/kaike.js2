a71b77fcafb891e7e8c754b13e2e54e82b7817a4
kaikediogo <kaikediogo2005@gmail.com>
2024-10-05 12:43:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function solicitardados() {
+//solicita o nome do usuario 
+let nome = prompt("qual e o seu nome?")
+//solicita a idade do usuario 
+let idade = prompt("qual e a sua idade?")
+//exibe a saudação
+alert("ola, " + nome + "! você tem " + idade + " anos.");
+// mostra os ddos mo console
+console.log("idade do usuario:", nome);
+console.log("idade do usuario:", idade);
+}
+
+
+
+
+
+
+
+
